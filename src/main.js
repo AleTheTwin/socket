c20fb84ec9a49f2b0e0ca734f9134bc4e93374a8
AleTheTwin <alethetwin@icloud.com>
2021-11-21 09:49:44 -0600

    select folder api

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const express = require('express')
+const fs = require('fs');
+
+const {dialog} = require('electron');
 
 const api = express()
 api.use(express.json())
@@ -10,7 +13,7 @@ var frame
 
 var isBusy = false
 
-function createViewport() {
+async function createViewport() {
     frame = new BrowserWindow({
         width : 900,
         height : 620,
@@ -68,3 +71,19 @@ api.get("/maximize", function(request, response) {
     })
 })
 
+api.get("/selectFolder", async function(request, response) {
+    let folderPath = await selectFolder()
+    let data = {
+        success : folderPath !== false ? true : false,
+        path: folderPath
+    }
+    response.send(data)
+})
+
+async function selectFolder() {
+    var directorio = await  dialog.showOpenDialog({
+        properties: ['openDirectory']
+    });
+    directorio = directorio.canceled ? undefined : path.resolve(directorio.filePaths[0]).replace(/\\/g, "/")
+    return directorio !== undefined ? directorio : false
+}
